70d5ee5438b11f00175da981549d492a9eeb6714
crazymisterno <crazymisterno@gmail.com>
2025-04-08 20:34:38 -0400

    Just in case, I'm making this scrollable

--- app/src/main/java/io/github/crazymisterno/GlucoseFit/ui/content/calendar/CalendarMain.kt
+++ app/src/main/java/io/github/crazymisterno/GlucoseFit/ui/content/calendar/CalendarMain.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -31,6 +33,7 @@ import java.time.ZoneOffset
 @Composable
 fun CalendarMain(navigate: (Int, Int, Int) -> Unit) {
     var state = rememberDatePickerState()
+    var scroll = rememberScrollState()
     Column(
         Modifier
             .fillMaxSize()
@@ -38,6 +41,7 @@ fun CalendarMain(navigate: (Int, Int, Int) -> Unit) {
                 MaterialTheme.colorScheme.primary,
                 MaterialTheme.colorScheme.secondary
             )))
+            .verticalScroll(scroll)
     ) {
         Text(
             text = "Calendar",
