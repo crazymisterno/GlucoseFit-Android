3ac041251b4fd562a591be691a891c45a2f82b97
crazymisterno <crazymisterno@gmail.com>
2025-04-13 00:12:59 -0400

    Move the app class to a more fitting place

--- app/src/main/java/io/github/crazymisterno/GlucoseFit/ui/content/MainActivity.kt
+++ app/src/main/java/io/github/crazymisterno/GlucoseFit/ui/content/MainActivity.kt
@@ -1,17 +1,14 @@
 package io.github.crazymisterno.GlucoseFit.ui.content
 
-import android.app.Application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.HiltAndroidApp
 import io.github.crazymisterno.GlucoseFit.ui.theme.GlucoseFitMaterialTheme
 
-@HiltAndroidApp
-class App : Application()
+
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
